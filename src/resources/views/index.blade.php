a79760e67f56bc180cd0c983c71f4f6aed827b36
yasuhito kitai <y.kitai.1124@gmail.com>
2025-01-09 21:57:11 +0900

    お気に入り登録機能実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -18,8 +18,28 @@
 <div class="item__content__group">
     @foreach($items_all as $item)
     <div class="item__content">
-        <div class="item-image">
-            <img src="{{$item->image}}" alt="item-image">
+        <div class="image__box">
+
+            <img class="image__box__item-image" src="{{$item->image}}" alt="item-image">
+
+            <form class="favorite-form" action="/favorite/{id}" method="post">
+                @csrf
+                @if (Auth::check())
+                @if ($favorites->where("user_id","=",$user_id)->where("item_id","=",$item["id"])->first())
+                <input type="hidden" name="item_id" value="{{$item->id}}">
+                <input class="favorite__on" type="image" src="img\bookmark_on.svg"></input>
+                @else
+                <input type="hidden" name="item_id" value="{{$item->id}}">
+                <input class="favorite__off" type="image" src="img\bookmark_off.svg"></input>
+                @endif
+                @else
+                <input type="hidden" name="item_id" value="{{$item->id}}">
+                <input class="favorite__off" type="image" src="img\bookmark_off.svg"></input>
+                @endif
+            </form>
+
+
+
         </div>
 
         <div class="item-name">{{$item->name}}</div>
